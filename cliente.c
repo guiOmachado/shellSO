389813fe6e5d1c503a98bee2da7ea44b132f061e
Lucas Costa <lucascostars@hotmail.com>
2019-12-12 18:12:52 -0300

    ultimo gas

--- cliente.c
+++ cliente.c
@@ -9,12 +9,10 @@
 
 #define REQ_QUEUE 	10010
 #define RESP_QUEUE 	10020
-
-
 #define MAX_TEXT_SIZE 	1000
 
 //Struct para enviar
-struct reqmsg {
+	struct reqmsg {
  	long cli_id;
 	char resposta_cli[MAX_TEXT_SIZE+1]; 
    	char comando[MAX_TEXT_SIZE+1];
@@ -22,8 +20,7 @@ struct reqmsg {
 	char caminho [MAX_TEXT_SIZE+1];
 	char texto_msg[MAX_TEXT_SIZE+1];
 	long id_destino;
-   	
-	
+	long cod_mansgem;
 };
 
 //Struct para receber
@@ -33,6 +30,26 @@ struct respmsg {
 	char resposta[MAX_TEXT_SIZE+1];
 };
 
+long int id_usuario(char init[8]){
+char atual;
+long count=0;
+int i = 0;
+
+for (i; i < strlen(init); i++)
+{if (init[i]=='a'){count = count + 250;}else if (init[i]=='b'){count = count + 575;}else if (init[i]=='c'){count = count + 20;}else if (init[i]=='d')
+{count = count + 95;}else if (init[i]=='e'){count = count + 43;}else if (init[i]=='f'){count = count + 81;}else if (init[i]=='g'){count = count + 888;}
+else if (init[i]=='h'){count = count + 5;}else if (init[i]=='i'){count = count + 34;}else if (init[i]=='j'){count = count + 475;}else if (init[i]=='k'){
+count = count + 121;}else if (init[i]=='l'){count = count + 1221;}else if (init[i]=='m'){count = count + 632;}else if (init[i]=='n'){count = count + 864;
+}else if (init[i]=='o'){count = count + 999;}else if (init[i]=='p'){count = count + 666;}else if (init[i]=='q'){count = count + 4123;}else if (init[i]=='r'){
+count = count + 71;}else if (init[i]=='s'){count = count + 562;}else if (init[i]=='t'){count = count + 324;}else if (init[i]=='u'){count = count + 101;
+}else if (init[i]=='v'){count = count + 21;}else if (init[i]=='x'){count = count + 17;}else if (init[i]=='z'){count = count + 13;}else if (init[i]=='0'){
+count = count + 2019;}else if (init[i]=='1'){count = count + 45;}else if (init[i]=='2'){count = count + 12;}else if (init[i]=='3'){count = count + 751;
+}else if (init[i]=='4'){count = count + 2101;}else if (init[i]=='5'){count = count + 1111;}else if (init[i]=='6'){count = count + 777;}else if (init[i]=='7')
+{count = count + 642;}else if (init[i]=='8'){count = count + 314;}else if (init[i]=='9'){count = count + 210;}else{count = count +0;}
+}
+return count;
+
+}
 
 void main()
 {
@@ -42,9 +59,9 @@ void main()
 	struct reqmsg cli_reqmsg;
 	struct respmsg serv_respmsg;
 	long cli_id;
-    int tempo;
-	FILE *arq;
+    	int tempo;
 	char *path;
+	char init[8];
 
 	system("clear");
 	printf("=================================================================\n");	
@@ -52,9 +69,19 @@ void main()
 	printf("===============DIGITE HELP PARA VER OS COMANDOS==================\n");
 	printf("============APERTE ENTER PARA EXECUTAR OS COMANADOS==============\n");	
 	printf("=================================================================\n");			
+/*
+	while(1){
+	printf("digite o Login: ");
+		scanf("%s", init);
+       scanf("%*c");
+	cli_reqmsg.cli_id = id_usuario(init);
+	//cli_reqmsg.cli_id = cli_id;
+	printf("%ld\n", cli_reqmsg.cli_id);
+	if (cli_reqmsg.cli_id=!0){break;}
+	printf("Login invalido\n");}
+	//cli_reqmsg.cli_id = cli_id;*/
 
 //Define o id para conexão
-
 	cli_id = getpid();
 	req_mq = msgget(REQ_QUEUE, 0777);
 
@@ -68,29 +95,35 @@ void main()
    		printf("msgget falhou no cliente\n"); 
 		exit(1); 
 	} 
+	/*
+	printf("%ld", cli_reqmsg.cli_id);
+	printf("%ld", cli_id);
+	
+	strcpy(cli_reqmsg.comando,"login");
+	msgsnd(req_mq,&cli_reqmsg,sizeof(struct reqmsg),0);
+	if (msgrcv(resp_mq,&serv_respmsg,sizeof(struct respmsg),cli_id,0) < 0){
+	printf("msgrcv falhou no cliente\n");
+	exit(1);}
+	printf("%s\n", serv_respmsg.resposta);*/
+
 	do {
+
+
         //Limpa
 		fflush(stdin);
 		fflush(stdout);
 		
 		//copia o path
+		
 		path = getcwd(path,0);  
 		strcpy(cli_reqmsg.path,path);     
-
+		
         //Define a mensagem inicial a ser exibida
-        printf("~ %s : ",path);
+        printf("~ %s: Entre com o Comando: ",path);
 		scanf("%[^\n]s",cli_reqmsg.comando);
        	scanf("%*c");
-
-		//verifica para mudar o diretório	
-		if(strcmp(cli_reqmsg.comando,"godir")==0||(cli_reqmsg.comando,"GODIR")==0){
-			printf("Digite o caminho \n");scanf("%[^\n]s",cli_reqmsg.caminho);scanf("%*c");chdir(cli_reqmsg.caminho);	
-		}		
-		if(strcmp(cli_reqmsg.comando,"logar")==0||(cli_reqmsg.comando,"LOGAR")==0){
-			 printf("~ %s : Digite seu id ",path);
-				scanf("%ld", &cli_id);
-       	        scanf("%*c");	
-		}	
+		
+		if(strcmp(cli_reqmsg.comando,"godir")==0||(cli_reqmsg.comando,"GODIR")==0){printf("Digite o caminho \n");scanf("%[^\n]s",cli_reqmsg.caminho);scanf("%*c");chdir(cli_reqmsg.caminho);}			
 		if(strcmp(cli_reqmsg.comando,"send")==0||(cli_reqmsg.comando,"SEND")==0||strcmp(cli_reqmsg.comando,"run")==0||(cli_reqmsg.comando,"RUN")==0){
 			 printf("~ %s : Digite sua mensagem: ",path);
 				scanf("%[^\n]s",cli_reqmsg.texto_msg);
@@ -100,6 +133,11 @@ void main()
 				scanf("%ld",&cli_reqmsg.id_destino);
        	        scanf("%*c");}
 		}
+		if(strcmp(cli_reqmsg.comando,"showmail")==0||(cli_reqmsg.comando,"SHOWMAIL")==0){
+		do{msgsnd(req_mq,&cli_reqmsg,sizeof(struct reqmsg),0);if (msgrcv(resp_mq,&serv_respmsg,sizeof(struct respmsg),cli_id,0) < 0){
+		printf("msgrcv falhou no cliente\n");exit(1);}printf("%s\n", serv_respmsg.resposta);}while(strcmp(serv_respmsg.resposta, "")!=0);
+				
+		}	
 		//copia o id
 			cli_reqmsg.cli_id = cli_id;
 
@@ -125,4 +163,3 @@ void main()
 
 	exit(0);	
 }
-
--- servidor.c
+++ servidor.c
@@ -24,6 +24,10 @@
 #define RESP_QUEUE 	10020
 #define MAX_TEXT_SIZE 	1000
 
+char concat[MAX_TEXT_SIZE+1];
+int  posUser=0;
+int  users[MAX_TEXT_SIZE+1];
+
 //Struct para receber
 struct reqmsg {
  	long cli_id;
@@ -34,26 +38,76 @@ struct reqmsg {
 	char texto_msg[MAX_TEXT_SIZE+1];	
 	long id_destino;
 	char aux[MAX_TEXT_SIZE+1];
+	long cod_mansgem;
 };
 //Struct para enviar
 struct respmsg {
 	long cli_id;
 	char resposta[MAX_TEXT_SIZE+1];
 };
+char * ls(char path[100])
+{
+		  memset (&concat, 0, sizeof (concat) );
+		  struct passwd *pw;
+		  struct group *gp;
+		  DIR *mydir;
+		  char *c;		  
+		  int i,linhaLs;
+		  struct dirent *myfile;
+		  struct stat fileStat;
+		  mydir=opendir(path);
+		  stat(".",&fileStat);
 
+ 	 while((myfile=readdir(mydir))!=NULL)
+ 	{
+		char stat1[MAX_TEXT_SIZE+1],stat2[MAX_TEXT_SIZE+1],stat3[MAX_TEXT_SIZE+1],stat4[MAX_TEXT_SIZE+1],stat5[MAX_TEXT_SIZE+1],stat6[MAX_TEXT_SIZE+1],stat7[MAX_TEXT_SIZE+1],stat8[MAX_TEXT_SIZE+1],stat9[MAX_TEXT_SIZE+1],stat10[MAX_TEXT_SIZE+1],stat11[MAX_TEXT_SIZE+1];
+			if(strcmp(myfile->d_name,".") != 0 && strcmp(myfile->d_name,"..") != 0)
+			{
+			    stat(myfile->d_name,&fileStat);  
+			    strcpy(stat1,(S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+			    strcpy(stat2,(fileStat.st_mode & S_IRUSR) ? "r" : "-");		    
+			    strcpy(stat3,(fileStat.st_mode & S_IWUSR) ? "w" : "-");
+			    strcpy(stat4,(fileStat.st_mode & S_IXUSR) ? "x" : "-");
+			    strcpy(stat5,(fileStat.st_mode & S_IRGRP) ? "r" : "-");
+			    strcpy(stat6,(fileStat.st_mode & S_IWGRP) ? "w" : "-");
+			    strcpy(stat7,(fileStat.st_mode & S_IXGRP) ? "x" : "-");
+			    strcpy(stat8,(fileStat.st_mode & S_IROTH) ? "r" : "-");
+			    strcpy(stat9,(fileStat.st_mode & S_IWOTH) ? "w" : "-");
+			    strcpy(stat10,(fileStat.st_mode & S_IXOTH) ? "x" : "-"); 
+				pw=getpwuid(fileStat.st_uid);
+		    	gp=getgrgid(fileStat.st_gid);			   
+			    strcat(concat,stat1);
+				strcat(concat,stat2);
+				strcat(concat,stat3);
+				strcat(concat,stat4);
+				strcat(concat,stat5);
+				strcat(concat,stat6);
+				strcat(concat,stat7);
+				strcat(concat,stat8);
+				strcat(concat,stat9);
+				strcat(concat,stat10);
+				strcat(concat," ");
+				strcat(concat,gp->gr_name);
+				strcat(concat," ");
+				strcat(concat,pw->pw_name);
+				strcat(concat," ");
+				strcat(concat,myfile->d_name);				
+				strcat(concat,"\n");		
+			}
+	}
+  closedir(mydir);  
+  return concat;
+}
 
 //Função da thread, thread que executa o comando 
 void *printMsg(struct reqmsg *cli_reqmsg) {
-	
-		
+
 		
 		if(strcmp(cli_reqmsg->comando,"dirlist")==0||strcmp(cli_reqmsg->comando,"DIRLIST")==0)
 		{
+			char *teste = ls(cli_reqmsg->path);
+			strcpy(cli_reqmsg->resposta_cli,teste);
 			
-			
-		} else if (strcmp(cli_reqmsg->comando,"clear")==0|| strcmp(cli_reqmsg->comando,"CLEAR")==0)
-		{
-			strcpy(cli_reqmsg->resposta_cli, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n=================================================================\n================SHELL SISTOPERACIONAIS INICIOU===================\n===============DIGITE HELP PARA VER OS COMANDOS==================\n============APERTE ENTER PARA EXECUTAR OS COMANADOS==============\n=================================================================\n");
 		}
 		else if(strcmp(cli_reqmsg->comando,"myid")==0|| strcmp(cli_reqmsg->comando,"MYID")==0)
 		{ 
@@ -67,7 +121,18 @@ void *printMsg(struct reqmsg *cli_reqmsg) {
 		}			
 		else if(strcmp(cli_reqmsg->comando,"exit")==0 || strcmp(cli_reqmsg->comando,"EXIT")==0)
 		{ 
-			exit(0);
+			int i;
+			for(i=0;i<10;i++)
+			{ 
+
+				if(users[i]==cli_reqmsg->cli_id)
+				{
+					users[i]=0;
+				}	
+			}
+
+			strcpy(cli_reqmsg->resposta_cli,"Cliente deslogado");	
+			kill(cli_reqmsg->cli_id, SIGINT);
 		}
 		else if(strcmp(cli_reqmsg->comando,"help")==0||strcmp(cli_reqmsg->comando,"HELP")==0)
 		{ 
@@ -75,41 +140,83 @@ void *printMsg(struct reqmsg *cli_reqmsg) {
 		}
 		else if(strcmp(cli_reqmsg->comando,"run")==0||strcmp(cli_reqmsg->comando,"RUN")==0)
 		{ 
-			
-			printf("\n\n%s\n\n", cli_reqmsg->aux);
-			long x = cli_reqmsg->cli_id;
-			sprintf(cli_reqmsg->resposta_cli, " gnome-terminal -e %s", cli_reqmsg->texto_msg);
-			system(cli_reqmsg->resposta_cli);
-			printf("\n\n%s\n\n", cli_reqmsg->resposta_cli);
-		
-			
+			char resp[MAX_TEXT_SIZE+1];
+			sprintf(resp, " gnome-terminal -x %s", cli_reqmsg->texto_msg);
+			sprintf(cli_reqmsg->resposta_cli,"\n~ %s : Executado com sucesso!!\n", cli_reqmsg->path);
+			system(resp);
 		}
 		else if(strcmp(cli_reqmsg->comando,"mail")==0||strcmp(cli_reqmsg->comando,"MAIL")==0)
 		{ 	
-		
-			
+			sprintf(cli_reqmsg->resposta_cli,"\n~ %s: Mensagem enviada!!\n", cli_reqmsg->path);
 		}
-		else if(strcmp(cli_reqmsg->comando,"showmail")==0||strcmp(cli_reqmsg->comando,"SHOWMAIL")==0)
-		{ 
-			
-		}
+		else if(strcmp(cli_reqmsg->comando,"showmail")==0||strcmp(cli_reqmsg->comando,"SHOWMAIL")==0){	sprintf(cli_reqmsg->resposta_cli,"\n~ %s: OK!!\n", cli_reqmsg->path);}
 		else if(strcmp(cli_reqmsg->comando,"send")==0||strcmp(cli_reqmsg->comando,"SEND")==0)
 		{ 
-			strcpy(cli_reqmsg->resposta_cli,"Mensagem enviada!!\n");
+			int login=0;
+			int i;
+			for(i=0;i<10;i++)
+			{ 
+				if(users[i]==cli_reqmsg->id_destino)
+				{
+					sprintf(cli_reqmsg->resposta_cli,"\n~ %s: Mensagem enviada!!\n", cli_reqmsg->path);
+					login=1;
+					break;
+				}	
+			}
+			if(login==0)
+			{
+				strcpy(cli_reqmsg->comando,"");
+				sprintf(cli_reqmsg->resposta_cli,"\n~ %s: Mensagem não pode ser enviada usuário não está logado!!\n", cli_reqmsg->path);
+			}
 
 		}
 		else if(strcmp(cli_reqmsg->comando,"receive")==0||strcmp(cli_reqmsg->comando,"RECEIVE")==0)
 		{ 
 			
 		}
+		else if(strcmp(cli_reqmsg->comando,"login")==0||strcmp(cli_reqmsg->comando,"LOGIN")==0)
+		{
+
+			int login=0;
+			int i;
+			for(i=0;i<10;i++)
+			{ 
+				if(users[i]==cli_reqmsg->cli_id)
+				{
+					strcpy(cli_reqmsg->resposta_cli,"O usuário ja esta logado!!\n");
+					login=1;
+					break;
+				}	
+			}
+			if(login==0)
+			{
+				users[posUser] = cli_reqmsg->cli_id;
+				sprintf(cli_reqmsg->resposta_cli,"\n~ %s: Logado seu id é %ld\n", cli_reqmsg->path, cli_reqmsg->cli_id);
+				posUser++; 
+			}
+		}
 		else if(strcmp(cli_reqmsg->comando,"users")==0||strcmp(cli_reqmsg->comando,"USERS")==0)
 		{ 
-			
+			int i=0;
+			char usersLogados[MAX_TEXT_SIZE+1];
+			char concatena[MAX_TEXT_SIZE+1];
+
+			strcpy(concatena,"Users logados: \n");
+			for(i=0;i<10;i++)
+			{ 
+				if(users[i]!=0)
+				{
+					snprintf( usersLogados, MAX_TEXT_SIZE+1 , "%d", users[i] );
+					strcat(concatena,usersLogados);
+					strcat(concatena,"\n");
+				}
+			}
+			strcpy(cli_reqmsg->resposta_cli,"");
+			strcpy(cli_reqmsg->resposta_cli,concatena);
+			memset (&concatena, 0, sizeof (concatena));
 		}else{
 			sprintf(cli_reqmsg->resposta_cli,"~ %s : Comando não encontrado!!\n", cli_reqmsg->path);
 		}
-	   	
-	
 }
 
 
@@ -157,17 +264,15 @@ void main()
 		msgsnd(resp_mq,&serv_respmsg,sizeof(struct respmsg),0);
 		printf("servidor: enviou resposta ao cliente %ld\n",serv_respmsg.cli_id);
 
-if (strcmp(cli_reqmsg.comando,"send")==0||(cli_reqmsg.comando,"SEND")==0)
-{
-	serv_respmsg.cli_id = cli_reqmsg.id_destino;
-	strcpy(serv_respmsg.resposta,cli_reqmsg.texto_msg);
-	msgsnd(resp_mq,&serv_respmsg,sizeof(struct respmsg),0);
-}
-
-		
-
+	if (strcmp(cli_reqmsg.comando,"send")==0||(cli_reqmsg.comando,"SEND")==0||strcmp(cli_reqmsg.comando,"mail")==0||strcmp(cli_reqmsg.comando,"MAIL")==0)
+	{
+		char teste[MAX_TEXT_SIZE+1];
+		sprintf(teste,"\n~ %s: Remetente %ld, a mensagem é: %s\n", cli_reqmsg.path, cli_reqmsg.cli_id, cli_reqmsg.texto_msg);
+		serv_respmsg.cli_id = cli_reqmsg.id_destino;
+		strcpy(serv_respmsg.resposta,teste);
+		msgsnd(resp_mq,&serv_respmsg,sizeof(struct respmsg),0);
+	}
 	}
 
 	exit(0);	
 }
-
